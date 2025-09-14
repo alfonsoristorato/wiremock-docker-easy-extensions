e6ea77e664d6458a523398c541788af73a2ac3e2
alfonsoristorato <alfonsoristorato@gmail.com>
2025-09-14 12:44:13 +0100

    add e2e tests

---
+++ src/test/kotlin/wdee/e2e/E2eDocker.kt
@@ -0,0 +1,5 @@
+package wdee.e2e
+
+// TODO implement e2e tests for starting the app via the DockerFile provided
+// tbd after the DockerFile does not depend on git anymore
+class E2eDocker
---
+++ src/test/kotlin/wdee/e2e/E2eJarBuild.kt
@@ -0,0 +1,84 @@
+package wdee.e2e
+
+import io.kotest.assertions.nondeterministic.eventually
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.utility.DockerImageName
+import org.testcontainers.utility.MountableFile
+import utils.HttpUtils
+import utils.TestUtils
+import java.io.File
+import kotlin.time.Duration.Companion.seconds
+
+class E2eJarBuild :
+    FunSpec({
+
+        val projectDir = File(".").canonicalFile
+        val e2eFilesDir = TestUtils.getConfigFileFromResources("e2e")
+        val configFile = File(e2eFilesDir, "wdee-config.yaml")
+        val wiremockMappingsDir = File(e2eFilesDir, "mappings")
+        val wiremockFilesDir = File(e2eFilesDir, "__files")
+        val wdeeBuiltJar = File(projectDir, "build/libs/wiremock-docker-easy-extensions.jar")
+        val processBuilder = ProcessBuilder()
+        val eventuallyWait = 30.seconds
+        lateinit var wdeeProcess: Process
+        lateinit var wiremockContainer: GenericContainer<*>
+
+        fun locateBuiltExtensionJar(): File = File(projectDir, "build/extensions/wiremock-extensions-bundled.jar")
+
+        fun wiremockRunning() = HttpUtils.get("http://localhost:${wiremockContainer.getMappedPort(8080)}/__admin/health")
+
+        beforeSpec {
+            wdeeProcess =
+                TestUtils.wdeeCommandHandlerAndLogger(
+                    processBuilder = processBuilder,
+                    wdeeBuiltJarPath = wdeeBuiltJar.absolutePath,
+                    command = "build",
+                    wdeeConfigFilePath = configFile.absolutePath,
+                    projectDir = projectDir,
+                )
+            eventually(eventuallyWait) {
+                wdeeProcess.waitFor() == 0
+            }
+
+            val extensionJar = locateBuiltExtensionJar()
+
+            wiremockContainer =
+                GenericContainer(DockerImageName.parse("wiremock/wiremock:3.13.1"))
+                    .withExposedPorts(8080)
+                    .withCopyFileToContainer(MountableFile.forHostPath(wiremockMappingsDir.absolutePath), "/home/wiremock/mappings")
+                    .withCopyFileToContainer(MountableFile.forHostPath(wiremockFilesDir.absolutePath), "/home/wiremock/__files")
+                    .withCopyFileToContainer(MountableFile.forHostPath(extensionJar.absolutePath), "/var/wiremock/extensions/")
+
+            wiremockContainer.start()
+
+            eventually(eventuallyWait) {
+                wiremockRunning()
+            }
+        }
+
+        afterSpec {
+            runCatching {
+                wiremockContainer.stop()
+                runCatching { wdeeProcess.destroy() }
+            }
+        }
+
+        fun endpoint(path: String) = "http://localhost:${wiremockContainer.getMappedPort(8080)}$path"
+
+        test("Kotlin no dependency transformer") {
+            HttpUtils.get(endpoint("/ResponseTransformerExtensionNoDependenciesKotlin")) shouldBe
+                "Response from ResponseTransformerExtensionNoDependenciesKotlin"
+        }
+
+        test("Java no dependency transformer") {
+            HttpUtils.get(endpoint("/ResponseTransformerExtensionNoDependenciesJava")) shouldBe
+                "Response from ResponseTransformerExtensionNoDependenciesJava"
+        }
+
+        test("Kotlin with dependency transformer") {
+            HttpUtils.get(endpoint("/ResponseTransformerExtensionWithDependenciesKotlin")) shouldBe
+                "Response from ResponseTransformerExtensionWithDependenciesKotlin using StringUtils from Apache Commons Lang3"
+        }
+    })
